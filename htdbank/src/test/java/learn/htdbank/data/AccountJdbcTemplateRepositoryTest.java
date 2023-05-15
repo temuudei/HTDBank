65bc9b213c9dda935fc302a9ddcaeddb838a6a99
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-05-15 11:54:11 -0700

    Not done

---
+++ htdbank/src/test/java/learn/htdbank/data/AccountJdbcTemplateRepositoryTest.java
@@ -0,0 +1,37 @@
+package learn.htdbank.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class AccountJdbcTemplateRepositoryTest {
+    @Autowired
+    AccountJdbcTemplateRepository repository;
+    @Autowired
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
