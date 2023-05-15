4d0075110d4b82fa4e3ce8cedf3a39b53c428b0b
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-05-15 09:42:25 -0700

    Added to data layer

--- htdbank/src/main/java/learn/htdbank/data/AccountJdbcTemplateRepository.java
+++ htdbank/src/main/java/learn/htdbank/data/AccountJdbcTemplateRepository.java
@@ -1,11 +1,16 @@
 package learn.htdbank.data;
 
 import learn.htdbank.data.mappers.AccountMapper;
+import learn.htdbank.data.mappers.CardMapper;
 import learn.htdbank.models.Account;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 @Repository
 public class AccountJdbcTemplateRepository implements AccountRepository {
@@ -28,28 +33,65 @@ public class AccountJdbcTemplateRepository implements AccountRepository {
     public Account findById(int account_id) {
         final String sql = "select account_id, customer_id, bank_id, account_balance "
                 + "from `Account` where account_id = ?;";
-
+        Account result = jdbcTemplate.query(sql, new AccountMapper(), account_id)
+                .stream()
+                .findFirst().orElse(null);
+        if (result != null) {
+            addCards(result);
+        }
+        return result;
     }
 
     @Override
     @Transactional
     public Account add(Account account) {
-        return null;
+        final String sql = "insert into `Account` (customer_id, bank_id, account_balance) values (?,?,?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, account.getCustomer_id());
+            ps.setInt(2, account.getBank_id());
+            ps.setBigDecimal(3, account.getBalance());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        account.setAccount_id(keyHolder.getKey().intValue());
+        return account;
     }
 
     @Override
     @Transactional
     public boolean update(Account account) {
-        return false;
+        final String sql = "update `Account` set "
+                + "customer_id = ?, "
+                + "bank_id = ?, "
+                + "account_balance = ? "
+                + "where account_id = ?;";
+        return jdbcTemplate.update(sql,
+                account.getCustomer_id(),
+                account.getBank_id(),
+                account.getBalance(),
+                account.getAccount_id()) > 0;
     }
 
     @Override
     @Transactional
     public boolean deleteById(int account_id) {
-        return false;
+        jdbcTemplate.update("delete from Card where account_id = ?;", account_id);
+        return jdbcTemplate.update("delete from `Account` where account_id = ?;", account_id) > 0;
     }
 
     private void addCards(Account account) {
-        final String sql = "select a.account_id, "
+        final String sql = "select a.account_id, a.customer_id, a.bank_id, a.account_balance, "
+                + "c.card_id, c.`type`, c.customer_id "
+                + "from Card c "
+                + "inner join `Account` a on c.account_id = a.account_id "
+                + "where c.account_id = ?;";
+        var accountCards = jdbcTemplate.query(sql, new CardMapper(), account.getAccount_id());
+        account.setCards(accountCards);
     }
 }
--- htdbank/src/main/java/learn/htdbank/data/BankJdbcTemplateRepository.java
+++ htdbank/src/main/java/learn/htdbank/data/BankJdbcTemplateRepository.java
@@ -1,4 +1,71 @@
 package learn.htdbank.data;
 
-public class BankJdbcTemplateRepository {
+import learn.htdbank.data.mappers.AccountMapper;
+import learn.htdbank.data.mappers.BankMapper;
+import learn.htdbank.models.Bank;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class BankJdbcTemplateRepository implements BankRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public BankJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public List<Bank> findAll() {
+        final String sql = "select bank_id, routing_number from Bank limit 1000;";
+        return jdbcTemplate.query(sql, new BankMapper());
+    }
+
+    @Override
+    @Transactional
+    public Bank findById(int bank_id) {
+        final String sql = "select bank_id, routing_number from Bank where bank_id = ?;";
+        Bank result = jdbcTemplate.query(sql, new BankMapper(), bank_id)
+                .stream()
+                .findFirst().orElse(null);
+        if (result != null) {
+            addEmployees(result);
+            addAccounts(result);
+        }
+        return result;
+    }
+
+    @Override
+    @Transactional
+    public Bank add(Bank account) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public boolean update(Bank account) {
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteById(int bank_id) {
+        return false;
+    }
+
+    private void addAccounts(Bank bank) {
+        final String sql = "select a.account_id, a.customer_id, a.bank_id, a.account_balance, "
+                + "b.routing_number "
+                + "from Account a "
+                + "inner join Bank b on a.bank_id = b.bank_id "
+                + "where a.bank_id = ?;";
+        var bankAccounts = jdbcTemplate.query(sql, new AccountMapper(), bank.getBank_id());
+        bank.setAccounts(bankAccounts);
+    }
+
+    private void addEmployees(Bank bank) {
+    }
 }
