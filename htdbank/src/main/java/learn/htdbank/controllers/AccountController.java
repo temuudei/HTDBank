5a388985c97a43d7c239ad20464d7f3c88ca6f4f
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-05-15 21:38:32 -0700

    Added controller classes

--- htdbank/src/main/java/learn/htdbank/controllers/AccountController.java
+++ htdbank/src/main/java/learn/htdbank/controllers/AccountController.java
@@ -1,12 +1,61 @@
 package learn.htdbank.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import learn.htdbank.domain.AccountService;
+import learn.htdbank.models.Account;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/account")
 public class AccountController {
+    private final AccountService service;
 
+    public AccountController(AccountService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Account> findAll() {
+        return service.findAll();
+    }
+    @GetMapping("/{account_id}")
+    public Account findById(@PathVariable int account_id) {
+        return service.findById(account_id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody @Valid Account account, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        service.add(account);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{account_id}")
+    public ResponseEntity<Object> update(@PathVariable int account_id, @RequestBody @Valid Account account, BindingResult result) {
+        if (account_id != account.getAccount_id()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        service.update(account);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{account_id}")
+    public ResponseEntity<Void> deleteById(@PathVariable int account_id) {
+        if (service.deleteById(account_id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- htdbank/src/main/java/learn/htdbank/controllers/BankController.java
+++ htdbank/src/main/java/learn/htdbank/controllers/BankController.java
@@ -1,4 +1,62 @@
 package learn.htdbank.controllers;
 
+import learn.htdbank.domain.BankService;
+import learn.htdbank.models.Bank;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/bank")
 public class BankController {
+    private final BankService service;
+
+    public BankController(BankService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Bank> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{bank_id}")
+    public Bank findById(@PathVariable int bank_id) {
+        return service.findById(bank_id);
+
+    }
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody @Valid Bank bank, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        service.add(bank);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{bank_id}")
+    public ResponseEntity<Object> update(@PathVariable int bank_id, @RequestBody @Valid Bank bank, BindingResult result) {
+        if (bank_id != bank.getBank_id()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        service.update(bank);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{bank_id}")
+    public ResponseEntity<Void> deleteById(@PathVariable int bank_id) {
+        if (service.deleteById(bank_id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
