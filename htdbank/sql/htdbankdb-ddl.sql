763057d51138b8ae7217b8c8cb5feb47a6f0e955
KeysGilbert <keysgilbert@gmail.com>
2023-05-17 14:49:45 -0400

    add tables for users and roles

--- htdbank/sql/htdbankdb-ddl.sql
+++ htdbank/sql/htdbankdb-ddl.sql
@@ -65,3 +65,28 @@ create table `Transaction`
     amount decimal(10,2) not null
 );
 
+create table app_user (
+    app_user_id int primary key auto_increment,
+    username varchar(50) not null unique,
+    password_hash varchar(2048) not null,
+    disabled bit not null default(0)
+);
+
+create table app_role (
+    app_role_id int primary key auto_increment,
+    `name` varchar(50) not null unique
+);
+
+create table app_user_role (
+    app_user_id int not null,
+    app_role_id int not null,
+    constraint pk_app_user_role
+        primary key (app_user_id, app_role_id),
+    constraint fk_app_user_role_user_id
+        foreign key (app_user_id)
+        references app_user(app_user_id),
+    constraint fk_app_user_role_role_id
+        foreign key (app_role_id)
+        references app_role(app_role_id)
+);
+
