0251378fec1304877bad455a48e1836dbb9c3be9
KeysGilbert <keysgilbert@gmail.com>
2023-05-17 14:52:44 -0400

    adjust test classes

--- htdbank/src/test/java/learn/htdbank/data/TransactionTemplateRepositoryTest.java
+++ htdbank/src/test/java/learn/htdbank/data/TransactionTemplateRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class TransactionTemplateRepositoryTest {
 
     @Autowired
--- htdbank/src/test/java/learn/htdbank/domain/CustomerServiceTest.java
+++ htdbank/src/test/java/learn/htdbank/domain/CustomerServiceTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 public class CustomerServiceTest {
 
     @Autowired
